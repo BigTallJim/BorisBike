c44cea52e8ea476a82d7dd89885fc66b40a04305
Volker Bergner <volker@bergner.one>
2019-08-21 17:31:37 +0100

    Chapter 16

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -25,10 +25,10 @@ describe DockingStation do
     expect(dock.release_bike).to eq (bike2)
     expect(dock.store).to eq ([bike1])
   end
-  
+
   it "test if exception thrown when docking station contains more than 20 bikes" do
     dock = DockingStation.new
-    20.times {dock.dock_bike(Bike.new)}
+    $DEFAULT_CAPACITY.times {dock.dock_bike(Bike.new)}
     expect {dock.dock_bike(Bike.new)}.to raise_error("Docking Station Full")
   end
 
