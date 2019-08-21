9deac5562495928b003e48c0cd10877eaea20f42
James Holton <james_holton@yahoo.co.uk>
2019-08-21 14:20:22 +0100

    first docking station commit

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,4 +1,7 @@
 require_relative "../lib/boris_bikes"
 
-describe DockingStation do
+describe DockingStation do  
+  it "expect release of bike" do
+    is_expected.to respond_to(DockingStation.release_bike)
+  end
 end
