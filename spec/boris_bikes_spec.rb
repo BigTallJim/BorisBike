2f0e60018484154127bd1429a25729a0d1a48b07
Student <student@Makerss-MacBook-Pro-2.local>
2019-08-21 14:26:10 +0100

    Chapter 8 done

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/boris_bikes"
 
-describe DockingStation do  
+describe DockingStation do
   it "expect release of bike" do
-    is_expected.to respond_to(DockingStation.release_bike)
+    is_expected.to respond_to(:release_bike)
   end
 end
