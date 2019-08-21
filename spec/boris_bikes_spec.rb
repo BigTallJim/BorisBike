fbf334672b5c12943076ffe67141ddb0db375b0e
James Holton <james_holton@yahoo.co.uk>
2019-08-21 15:00:53 +0100

    Added working method to Bike

--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
   it "expect release of bike" do
     is_expected.to respond_to(:release_bike)
   end
+  it "test bike is returned from release bike" do
+    expect(DockingStation.new.release_bike.working?).to eq (true)
+  end
 end
